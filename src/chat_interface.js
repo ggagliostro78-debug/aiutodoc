ef3ac247870106f8df3464f8e6254da4eadcd249
gaglippe <ggagliostro78@gmail.com>
2026-05-14 02:24:56 +0200

    Restore mobile chat scroll container

--- src/chat_interface.js
+++ src/chat_interface.js
@@ -363,12 +363,10 @@ class ChatInterface {
             const lastMessage = this.messagesContainer.lastElementChild;
 
             if (window.innerWidth <= 950) {
-                const inputStyle = this.inputArea ? window.getComputedStyle(this.inputArea) : null;
-                const inputVisible = inputStyle && inputStyle.display !== 'none' && inputStyle.visibility !== 'hidden';
-                const target = inputVisible ? this.inputArea : lastMessage;
-                if (target) {
-                    target.scrollIntoView({ behavior: 'auto', block: 'end' });
-                }
+                this.messagesContainer.scrollTo({
+                    top: this.messagesContainer.scrollHeight,
+                    behavior: 'auto'
+                });
                 return;
             }
 
